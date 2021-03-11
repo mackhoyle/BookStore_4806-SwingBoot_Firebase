4e0ae0aa05148c2d2fc3cadd78ce9ddefba18b83
Sam <SamBaumann@cmail.carlteon.ca>
2021-03-11 16:59:29 -0500

    Added Book model & Bookstore controller

--- src/main/java/com/app/models/Book.java
+++ src/main/java/com/app/models/Book.java
@@ -1,2 +1,30 @@
-package com.app.models;public class Book {
+package com.app.models;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Book extends Model {
+    private String name;
+    private String author;
+    private String img;
+    private String genre;
+    private String isbn;
+    private int cost;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getImg() { return img; }
+
+    public String getGenre() { return genre; }
+
+    public int getCost() { return cost;}
+
+    public String getIsbn() { return isbn;}
+
 }
