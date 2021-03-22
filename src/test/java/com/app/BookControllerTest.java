7ce1cd9827ec7a42ab36df4f24c6d1f1c857655e
mackhoyle <48255736+mackhoyle@users.noreply.github.com>
2021-03-21 21:17:56 -0400

    Refactored tests

--- src/test/java/com/app/BookControllerTest.java
+++ src/test/java/com/app/BookControllerTest.java
@@ -14,7 +14,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class BookControllerTest {
+public class  BookControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
--- src/test/java/com/app/LoginControllerTest.java
+++ src/test/java/com/app/LoginControllerTest.java
@@ -6,8 +6,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.ui.Model;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
@@ -27,13 +31,6 @@ public class LoginControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("index"));
     }
-    @Test
-    public void testLoginSuccessful() throws Exception {
-        mockMvc.perform(get("/login")
-                .requestAttr("loginPassword", "admin")
-                .requestAttr("loginEmail", "john_doe@gmail.com"))
-                .andExpect(view().name("loginSuccess"));
 
-    }
 
 }
